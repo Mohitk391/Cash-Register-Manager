e49ba73c31d45735301100f3c5002550adfc0915
Mohitk391 <mohitkpatel391@gmail.com>
2021-10-14 19:10:04 +0530

    Updated backend

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ var twenty = document.querySelector("#twenty");
 var ten = document.querySelector("#ten");
 var five = document.querySelector("#five");
 var one = document.querySelector("#one");
+var equalAmount = document.querySelector("#equalAmount");
+
 
 var returnCash;
 twoThousand.innerText = "--";
@@ -20,52 +22,61 @@ one.innerText = "--";
 
 
 function ClickEventHandler(){
+    document.querySelectorAll("input").forEach((element) => { element.disabled = true; });
     returnCash = cashGiven.value - billAmount.value;
-    var flag= true;
-    
-    while(flag){
-        if(returnCash===0){
-            flag=false;
-            break;
-        }
-        else {
-            if(returnCash >= 2000){
-                twoThousand.innerText = Math.floor(returnCash / 2000) ;
-                returnCash = returnCash % 2000 ;
-                continue;
+    if(returnCash < 0){
+        equalAmount.innerHTML = "Cash Given is less than the bill amount.<br> Amount still required: Rs. <b>" +  Math.abs(returnCash) + "</b>.";
+    }
+    else if(returnCash === 0) {
+        equalAmount.innerText = "No change is required to be given. Customer has given required amount of money.";
+    }
+    else{
+        equalAmount.innerHTML = "";
+        var flag= true;
+        
+        while(flag){
+            if(returnCash===0){
+                flag=false;
+                break;
             }
-            else if(returnCash >= 500) {
-                fiveHundred.innerText = Math.floor(returnCash / 500);
-                returnCash = returnCash % 500;
-                continue;
-            }
-            else if(returnCash >= 100) {
-                oneHundred.innerText = Math.floor(returnCash / 100);
-                returnCash = returnCash % 100;
-                continue;
-            }
-            else if(returnCash >= 20) {
-                twenty.innerText = Math.floor(returnCash / 20);
-                returnCash = returnCash % 20;
-                continue;
-            }
-            else if(returnCash >= 10) {
-                ten.innerText = Math.floor(returnCash / 10);
-                returnCash = returnCash % 10;
-                continue;
-            }
-            else if(returnCash >= 5) {
-                five.innerText = Math.floor(returnCash / 5);
-                returnCash = returnCash % 5;
-                continue;
-            }
-            else if(returnCash >= 1) {
-                one.innerText = Math.floor(returnCash / 1);
-                returnCash = returnCash % 1;
+            else {
+                if(returnCash >= 2000){
+                    twoThousand.innerText = Math.floor(returnCash / 2000) ;
+                    returnCash = returnCash % 2000 ;
+                    continue;
+                }
+                else if(returnCash >= 500) {
+                    fiveHundred.innerText = Math.floor(returnCash / 500);
+                    returnCash = returnCash % 500;
+                    continue;
+                }
+                else if(returnCash >= 100) {
+                    oneHundred.innerText = Math.floor(returnCash / 100);
+                    returnCash = returnCash % 100;
+                    continue;
+                }
+                else if(returnCash >= 20) {
+                    twenty.innerText = Math.floor(returnCash / 20);
+                    returnCash = returnCash % 20;
+                    continue;
+                }
+                else if(returnCash >= 10) {
+                    ten.innerText = Math.floor(returnCash / 10);
+                    returnCash = returnCash % 10;
+                    continue;
+                }
+                else if(returnCash >= 5) {
+                    five.innerText = Math.floor(returnCash / 5);
+                    returnCash = returnCash % 5;
+                    continue;
+                }
+                else if(returnCash >= 1) {
+                    one.innerText = Math.floor(returnCash / 1);
+                    returnCash = returnCash % 1;
+                }
             }
         }
     }
-    
 }
 
 
